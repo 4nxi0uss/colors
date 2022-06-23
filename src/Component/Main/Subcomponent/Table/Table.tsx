ac0d82e4b958d120327b34c6cbddc81a1eb21b05
Artur Zaczek <artur200212@interia.pl>
2022-06-23 07:33:07 +0200

    start dooing pagination,

--- src/Component/Main/Subcomponent/Table/Table.tsx
+++ src/Component/Main/Subcomponent/Table/Table.tsx
@@ -1,33 +1,76 @@
-import { useGetProductsQuery } from '../../../../Redux/ColorsApi/ProductsApi';
+import { useState } from 'react';
+
 import './Table.scss'
+
+import { useGetProductsQuery, useNextPageMutation } from '../../../../Redux/ColorsApi/ProductsApi';
 
 const Table = () => {
 
-    const { data, error, isLoading, isError } = useGetProductsQuery(null);
+    const [pageNumber, setPageNumber] = useState<number>(2);
+
+    const { data, error, isLoading, isError } = useGetProductsQuery(pageNumber);
+
+    const [updatePages, result] = useNextPageMutation();
+
+    console.log("dara", data)
+    console.log("rws", result)
 
     isError && console.warn(error)
 
     const showTableColors = () => !isLoading && data?.data.map((el: any) => (
-        <tr style={{ backgroundColor: el.color }}>
+        <tr key={el.id} style={{ backgroundColor: el.color }}>
             <td>{el.id}</td>
             <td>{el.name}</td>
             <td>{el.year}</td>
         </tr>
     ));
 
+    const handleChangePagePrev = (e: any) => {
+        e.preventDefault()
+
+        if (pageNumber <= 1) {
+            setPageNumber(1);
+        } else {
+            setPageNumber((state) => (state -= 1))
+        }
+    }
+
+    const handleChangePageNext = (e: any) => {
+        e.preventDefault()
+
+        if (pageNumber >= data?.total_pages) {
+            setPageNumber(data?.total_pages);
+        } else {
+            setPageNumber((state) => (state += 1))
+        }
+    }
+
+    const paginationDiv = () => { updatePages(3) };
+
+    console.log(pageNumber);
     return (
-        <table className='table'>
-            <thead className='table__head'>
-                <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Year</th>
-                </tr>
-            </thead>
-            <tbody className='table__body'>
-                {showTableColors()}
-            </tbody>
-        </table>
+        <section>
+            <table className='table'>
+                <thead className='table__head'>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Year</th>
+                    </tr>
+                </thead>
+                <tbody className='table__body'>
+                    {showTableColors()}
+                </tbody>
+            </table>
+            <div className='pagination'>
+                <button className='pagination__btn pagination__btn--prev' onClick={handleChangePagePrev}>Prev</button>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <button className='pagination__btn pagination__btn--next' onClick={handleChangePageNext}>Next</button>
+                <button className='pagination__next' onClick={paginationDiv}>test</button>
+            </div>
+        </section>
     )
 }
 
--- src/Redux/ColorsApi/ProductsApi.ts
+++ src/Redux/ColorsApi/ProductsApi.ts
@@ -3,14 +3,23 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const productsApi = createApi({
     reducerPath: 'productsApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://reqres.in/api/products' }),
+    tagTypes: ["Page"],
     endpoints: (builder) => ({
-        getProducts: builder.query<any, any>({
-            query: () => ({
-                url: "/?per_page=5",
+        getProducts: builder.query<any, number>({
+            query: (page) => ({
+                url: `/?page=${page}&per_page=5`,
                 method: 'GET',
             }),
+            providesTags: ["Page"]
+        }),
+        nextPage: builder.mutation<any, number>({
+            query: (page) => ({
+                url: `/?page=${page}&per_page=5`,
+                method: 'GET',
+            }),
+            invalidatesTags: ["Page"]
         }),
     }),
 })
 
-export const { useGetProductsQuery } = productsApi
+export const { useGetProductsQuery, useNextPageMutation } = productsApi
